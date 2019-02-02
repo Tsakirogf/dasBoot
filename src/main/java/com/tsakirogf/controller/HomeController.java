31062ff9afe7d479ae67d52e830a9fb6c5e9919c
Fotis Tsakiroglou <tsakirogf@hotmail.com>
2019-02-02 11:30:37 +0000

    End of course - Starting customizing

--- src/main/java/com/tsakirogf/controller/HomeController.java
+++ src/main/java/com/tsakirogf/controller/HomeController.java
@@ -2,13 +2,19 @@ package com.tsakirogf.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @RestController
 public class HomeController
 {
-    @RequestMapping("/")
+    @RequestMapping("/home")
     public String home()
     {
         return "Das Boot, reporting for duty.";
+    }
+
+    public void addViewControllers(ViewControllerRegistry registry)
+    {
+        registry.addViewController("/").setViewName("redirect:/index.html");
     }
 }
--- src/test/java/com/tsakirogf/AppTest.java
+++ src/test/java/com/tsakirogf/AppTest.java
@@ -1,7 +1,9 @@
 package com.tsakirogf;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import com.tsakirogf.controller.HomeController;
 import org.junit.Test;
 
 /**
@@ -13,8 +15,9 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void testHome()
     {
-        assertTrue( true );
+        HomeController hc = new HomeController();
+        assertEquals("Das Boot, reporting for duty.", hc.home());
     }
 }
